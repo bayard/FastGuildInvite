5a2c51b9c21dd9859a96f9ee2e8db8b056c35e66
Knoot <areuar@mail.ru>
2019-02-27 09:35:50 +0300

    fix bug
    Fixed bug with sending messages in invitation mode 1 and 2

--- functions.lua
+++ functions.lua
@@ -271,8 +271,18 @@ function fn:invitePlayer(noInv)
 	else
 		list = addon.search.inviteList
 		if #list==0 then return end
-		if not noInv then
+		--[[if not noInv then
 			GuildInvite(list[1].name)
+			DB.alredySended[list[1].name] = time({year = date("%Y"), month = date("%m"), day = date("%d")})
+		end]]
+		if (DB.inviteType == 1 or DB.inviteType == 2) and not noInv then
+			GuildInvite(list[1].name)
+		end
+		if (DB.inviteType == 2 or DB.inviteType == 3) and not noInv then
+			local msg = DB.messageList[DB.curMessage]
+			sendWhisper(msg, list[1].name)
+		end
+		if not noInv then
 			DB.alredySended[list[1].name] = time({year = date("%Y"), month = date("%m"), day = date("%d")})
 		end
 		table.remove(addon.search.inviteList, 1)
