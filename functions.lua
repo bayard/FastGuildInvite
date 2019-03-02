1a00df7b384cd4d501b3c0b379ea441538b464d6
Knoot <areuar@mail.ru>
2019-03-03 00:24:13 +0300

    Removed invitation delay in invitation mode 2

--- functions.lua
+++ functions.lua
@@ -254,7 +254,7 @@ function fn:invitePlayer(noInv)
 	local list = DB.SearchType == 3 and addon.smartSearch.inviteList or addon.search.inviteList
 	if #list==0 then return end
 	if (DB.inviteType == 1 or DB.inviteType == 2) and not noInv then
-		C_Timer.After(DB.inviteType == 2 and 5 or 0, function() GuildInvite(list[1].name) end)
+		GuildInvite(list[1].name)
 	end
 	if (DB.inviteType == 2 or DB.inviteType == 3) and not noInv then
 		local msg = DB.messageList[DB.curMessage]
